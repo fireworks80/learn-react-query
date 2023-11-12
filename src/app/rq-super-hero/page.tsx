a7b750d129b689f73aeadc0afdd9e7bc21d71d80
fireworks <seungchulhan@gmail.com>
2023-11-12 16:38:13 +0900

    feat: handling error

--- src/app/rq-super-hero/page.tsx
+++ src/app/rq-super-hero/page.tsx
@@ -6,9 +6,14 @@ import { Hero } from '../super-hero/page';
 const fetchSuperHeroes = () => axios.get('http://localhost:4000/superheroes');
 
 export default function RqSuperHero() {
-	const { isLoading, data } = useQuery('super-heros', fetchSuperHeroes);
+	const { isLoading, data, isError, error } = useQuery(
+		'super-heros',
+		fetchSuperHeroes
+	);
 
 	if (isLoading) return <h2>Loading ...</h2>;
+
+	if (isError) return <h2>{error?.message}</h2>;
 
 	return (
 		<>
--- src/app/super-hero/page.tsx
+++ src/app/super-hero/page.tsx
@@ -12,16 +12,27 @@ export type Hero = {
 export default function SuperHero() {
 	const [isLoading, setIsLoading] = useState(true);
 	const [data, setData] = useState<Array<Hero>>([]);
+	const [error, setError] = useState('');
 
 	useEffect(() => {
-		axios.get('http://localhost:4000/superheroes').then((res) => {
-			setData(res.data);
-			setIsLoading(false);
-		});
+		axios
+			.get('http://localhost:4000/superheroes')
+			.then((res) => {
+				setData(res.data);
+				setIsLoading(false);
+			})
+			.catch((error) => {
+				setError(error.message);
+				setIsLoading(false);
+			});
 	}, []);
 
 	if (isLoading) {
 		return <h2>Loading ...</h2>;
+	}
+
+	if (error) {
+		return <h2>{error}</h2>;
 	}
 
 	return (
